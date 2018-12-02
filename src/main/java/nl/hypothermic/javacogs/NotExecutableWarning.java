c63f5bfceb7246e9cb0e0b9ad79fedc07c35d771
hypothermic <admin@hypothermic.nl>
2018-12-02 17:31:40 +0100

    Add warning when run as executable

---
+++ src/main/java/nl/hypothermic/javacogs/NotExecutableWarning.java
@@ -0,0 +1,8 @@
+package nl.hypothermic.javacogs;
+
+public class NotExecutableWarning {
+	
+	public static void main(String[] args) {
+		System.out.println("ERROR: Javacogs is a library and cannot be run as executable. Exiting.");
+	}
+}
