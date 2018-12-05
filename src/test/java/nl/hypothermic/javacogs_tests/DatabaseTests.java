0faa550da68be07c452fa22e0b81a535bb5c539b
hypothermic <admin@hypothermic.nl>
2018-12-05 09:20:30 +0100

    Add unit test for getMasterById()

--- src/test/java/nl/hypothermic/javacogs_tests/DatabaseTests.java
+++ src/test/java/nl/hypothermic/javacogs_tests/DatabaseTests.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import nl.hypothermic.javacogs.Javacogs;
 import nl.hypothermic.javacogs.ResponseCallback;
+import nl.hypothermic.javacogs.entities.Master;
 import nl.hypothermic.javacogs.entities.Release;
 import nl.hypothermic.javacogs.handlers.Handler;
 import nl.hypothermic.javacogs.network.Response;
@@ -27,7 +28,27 @@ public class DatabaseTests {
 					System.out.println(response.getValue().toString());
 					Assert.assertEquals(response.getValue().getReleaseYear(), 1987);
 				} else {
-					Assert.fail("Response failed.");
+					Assert.fail("DatabaseTests: getReleaseByIdTest: Response failed.");
+				}
+			}
+		});
+    }
+	
+	/**
+	 * Fails if Javacogs couldn't get/parse response:
+	 * <pre><code>if (response.hasSucceeded()) {</code></pre>
+	 * or the response was invalid: (
+	 * <pre><code>Assert.assertEquals(response.getValue().getReleaseYear(), 1987);</code></pre>
+	 */
+	@Test
+    public void getMasterByIdTest() throws IOException {
+        Javacogs.getInstance().getHandler(Handler.DATABASE).getMasterById(1000, new ResponseCallback<Master>() {
+			public void onResult(Response<Master> response) {
+				if (response.hasSucceeded()) {
+					System.out.println(response.getValue().toString());
+					Assert.assertEquals(response.getValue().getReleaseYear(), 1997);
+				} else {
+					Assert.fail("DatabaseTests: getMasterByIdTest: Response failed.");
 				}
 			}
 		});
