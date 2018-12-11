38c9f71660ff0ee3e45433ddb294cc145d6f5f2a
hypothermic <admin@hypothermic.nl>
2018-12-11 18:17:57 +0100

    Add documentation to token/key/secret authentication methods

--- src/main/java/nl/hypothermic/javacogs/authentication/KeySecretAuthenticationMethod.java
+++ src/main/java/nl/hypothermic/javacogs/authentication/KeySecretAuthenticationMethod.java
@@ -8,6 +8,17 @@ public class KeySecretAuthenticationMethod implements AuthenticationMethod {
 	
 	private final String key, secret;
 	
+	/**
+	 * Also known as the Discogs Auth Flow.<br>
+	 * <br>
+	 * This class simply holds the user's <code>key</code> and <code>secret</code> and appends a Authorization header to the HttpsURLConnection.<br>
+	 * <br>
+	 * For the token method, see {@link nl.hypothermic.javacogs.authentication.KeySecretAuthenticationMethod#KeySecretAuthenticationMethod(String, String)}
+	 * 
+	 * @see <a href="https://www.discogs.com/developers/#page:authentication,header:authentication-discogs-auth-flow">
+	 *          https://www.discogs.com/developers/#page:authentication,header:authentication-discogs-auth-flow
+	 *      </a>
+	 */
 	public KeySecretAuthenticationMethod(final String key, final String secret) {
 		if (key == null || secret == null) {
 			throw new IllegalArgumentException("Key and/or secret cannot be null.");
--- src/main/java/nl/hypothermic/javacogs/authentication/TokenAuthenticationMethod.java
+++ src/main/java/nl/hypothermic/javacogs/authentication/TokenAuthenticationMethod.java
@@ -8,6 +8,17 @@ public class TokenAuthenticationMethod implements AuthenticationMethod {
 	
 	private final String token;
 	
+	/**
+	 * Also known as the Discogs Auth Flow.<br>
+	 * <br>
+	 * This class simply holds the user's <code>token</code> and appends a Authorization header to the HttpsURLConnection.<br>
+	 * <br>
+	 * For the key/secret method, see {@link nl.hypothermic.javacogs.authentication.KeySecretAuthenticationMethod#KeySecretAuthenticationMethod(String, String)}
+	 * 
+	 * @see <a href="https://www.discogs.com/developers/#page:authentication,header:authentication-discogs-auth-flow">
+	 *          https://www.discogs.com/developers/#page:authentication,header:authentication-discogs-auth-flow
+	 *      </a>
+	 */
 	public TokenAuthenticationMethod(final String token) {
 		if (token == null) {
 			throw new IllegalArgumentException("Token cannot be null.");
