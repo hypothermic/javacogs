6421b2819ce36902797550ee4d9928bb9a391cbb
hypothermic <admin@hypothermic.nl>
2018-12-28 18:22:43 +0100

    Update javadoc link for SearchResult

--- src/main/java/nl/hypothermic/javacogs/entities/SearchResult.java
+++ src/main/java/nl/hypothermic/javacogs/entities/SearchResult.java
@@ -9,7 +9,7 @@ import nl.hypothermic.javacogs.constants.EntityType;
  * This is a wrapper for any search result which can be crafted
  * into an Entity (Release, Master, Label, ArtistWrapper, etc.)
  * <br>
- * See {@link nl.hypothermic.javacogs.entities.SearchResult#toEntity()}
+ * See {@link nl.hypothermic.javacogs.handlers.DatabaseHandler#getEntityFromSearchResult(SearchResult, nl.hypothermic.javacogs.concurrency.UncheckedCallback)}
  */
 public class SearchResult {
 	
